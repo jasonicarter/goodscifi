cb2a3b7f72a7af26d26a83d009edc73eff876de3
Jason Carter <jason.ian.carter@gmail.com>
2017-06-19 23:40:30 -0400

    Add world_without_end spider with image download

--- src/data/webcrawler/scrapy_spiders/spiders/world_without_end.py
+++ src/data/webcrawler/scrapy_spiders/spiders/world_without_end.py
@@ -1,22 +1,42 @@
+import os
 import scrapy
 from scrapy.exceptions import CloseSpider
 from scrapy_spiders.pipelines import clean_title
+from dotenv import load_dotenv, find_dotenv
 
 class SpiderWwEnd(scrapy.Spider):
     name='wwend_com'
 
+    # Find .env automagically by walking up directories until it's found
+    # Load up the entries as environment variables
+    load_dotenv(find_dotenv())
+    RAW_DATA_PATH = os.environ.get("RAW_DATA_PATH")
+
+    # Used for downloading images
+    # Refactor: pipelines files could be moved into individual spiders
+    custom_settings = {
+        'ITEM_PIPELINES': {'scrapy.pipelines.images.ImagesPipeline': 1},
+        'IMAGES_STORE': RAW_DATA_PATH+'/images',
+        'CONCURRENT_REQUESTS': 1,
+        'DOWNLOAD_DELAY': 1,
+        }
+
     def start_requests(self):
-        for year_published in range(2017,2018,1):
+        for year_published in range(1930,1932,1):
             yield scrapy.Request(\
             'https://www.worldswithoutend.com/lists_booksbyyear.asp?p=1&YearPublished=%s&Genre=SF&NovelType=1&Nominated='\
             % year_published)
 
     def parse(self, response):
         for title in response.xpath('//div[contains(@id,"reportlist")]/table/tr/td/table/td'):
+
             yield {
                 'title': clean_title(title.css('table tr td p a::text').extract_first()),
-                # Using url obtained from pagination - index should be okay to use
-                'year': response.url[response.url.find('YearPublished=')+14:response.url.find('&Genre=')]
+                # Using url obtained from pagination html - index should be okay
+                'year': response.url[
+                    response.url.find('YearPublished=')+14:response.url.find('&Genre=')],
+                'image_urls': ['https://www.worldswithoutend.com/'+\
+                    title.css('table tr div.awardslisting img::attr(src)').extract_first()]
             }
 
         # follow pagination links
