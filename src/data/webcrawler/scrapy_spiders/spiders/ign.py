7fe431591287b0f2d89b9058197e3d50db5be6d5
Jason Carter <jason.ian.carter@gmail.com>
2017-06-22 15:03:24 -0400

    Fix custom settings typo

--- src/data/webcrawler/scrapy_spiders/spiders/ign.py
+++ src/data/webcrawler/scrapy_spiders/spiders/ign.py
@@ -13,7 +13,7 @@ class SpiderIGN(scrapy.Spider):
 
     # Site blocks scrapy
     custom_settings = {
-        'ITEM_PIPELINES': {'scrapy_spiders.pipelines.IgnItemPipeline':300}
+        'ITEM_PIPELINES': {'scrapy_spiders.pipelines.IgnItemPipeline':300},
         'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
         'CONCURRENT_REQUESTS': 1,
         'DOWNLOAD_DELAY': 1,
