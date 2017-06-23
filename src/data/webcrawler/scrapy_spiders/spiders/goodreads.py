9819bdb1f90e9f586c2ec06a5bfc8d581f0130b0
Jason Carter <jason.ian.carter@gmail.com>
2017-06-23 09:50:32 -0400

    Fix typo with start_urls

--- src/data/webcrawler/scrapy_spiders/spiders/goodreads.py
+++ src/data/webcrawler/scrapy_spiders/spiders/goodreads.py
@@ -6,11 +6,11 @@ class SpiderGoodReads(scrapy.Spider):
 
     start_urls = [
         'https://www.goodreads.com/list/show/4893.Best_Science_Fiction_of_the_21st_Century?page=1',
-        'https://www.goodreads.com/list/show/4893.Best_Science_Fiction_of_the_21st_Century?page=2'
+        'https://www.goodreads.com/list/show/4893.Best_Science_Fiction_of_the_21st_Century?page=2',
         'https://www.goodreads.com/list/show/35776.Most_Popular_Science_Fiction_on_goodreads_with_over_50_thousand_ratings?page=1',
-        'https://www.goodreads.com/list/show/35776.Most_Popular_Science_Fiction_on_goodreads_with_over_50_thousand_ratings?page=2'
+        'https://www.goodreads.com/list/show/35776.Most_Popular_Science_Fiction_on_goodreads_with_over_50_thousand_ratings?page=2',
         'https://www.goodreads.com/list/show/19341.Best_Science_Fiction?page=1',
-        'https://www.goodreads.com/list/show/19341.Best_Science_Fiction?page=2'
+        'https://www.goodreads.com/list/show/19341.Best_Science_Fiction?page=2',
     ]
 
     custom_settings = {
