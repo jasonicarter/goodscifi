4df7cbe331af7453edbc8990f550ba013d4a3ace
Jason Carter <jason.ian.carter@gmail.com>
2017-07-08 10:41:22 -0400

    Add CORS package

--- google_cloud_platform/gce_flask_api/api/main.py
+++ google_cloud_platform/gce_flask_api/api/main.py
@@ -1,17 +1,15 @@
-from flask import Flask, current_app, request, jsonify, make_response, Blueprint
+from flask import Flask, current_app, request, jsonify, make_response
 import io
 import model
 import base64
 import logging
 
-# from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
-api = Blueprint('api', app, url_prefix='/api/v1')
-
-@api.route('/books', methods=['POST'])
+@app.route('/api/v1/books', methods=['POST'])
 def predict():
 
     if request.headers['Content-Type'] != 'application/json':
@@ -31,7 +29,7 @@ def predict():
 
     return jsonify(predictions=predictions)
 
-@api.errorhandler(404)
+@app.errorhandler(404)
 def not_found(error):
     return jsonify(status_code='404', msg='Not found'), 404
 
@@ -43,7 +41,6 @@ POST api.goodscifi.com/v1/books/ #returns book_id
 GET api.goodscifi.com/v1/books/<book_id>/predictions #returns predictions
 GET api.goodscifi.com/v1/movies/predictions?filter_by=recent
 '''
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
